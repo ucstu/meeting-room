96ad400ba18c95519cdab266825941b6877b170c
张旭东 <2219454275@qq.com>
2025-07-31 17:35:33 +0800

    🐳 chore: 添加提交类型检查

--- lint-staged.config.ts
+++ lint-staged.config.ts
@@ -1,6 +1,7 @@
 import type { Configuration } from "lint-staged";
 
 export default {
+  "*.{ts,tsx,vue}": () => "nuxt typecheck",
   "*.{js,ts,jsx,tsx,vue}": ["eslint --fix", "prettier --write"],
   "*.{vue,html,css,scss,sass}": ["stylelint --fix", "prettier --write"],
   "*.{json,md,yml}": "prettier --write",
